667abb4ddda1668bf20e7b7eded8db2451b28919
Nicola Casarin <nico.casarin@icloud.com>
2022-02-10 16:05:38 +0100

    for loop for titles

--- scripts/my_script.R
+++ scripts/my_script.R
@@ -2,9 +2,13 @@ library(tidyverse)
 load('s3-simpsons.Rdata')
 
 str(simpsons)
-
+length(simpsons)
 i = 1
 while (i <= length(simpsons)) {
   print(simpsons[[i]][['title']])
   i <- i+1 
+}
+
+for (i in seq_along(simpsons)) {
+  cat(simpsons[[i]]$title, '\n')
 }
